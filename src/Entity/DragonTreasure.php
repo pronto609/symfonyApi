c203e62ed5b78122687a1e155326a842fd733d1a
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-16 08:11:17 +0300

    add property abolity to be shown and wrtitable

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -12,6 +12,7 @@ use App\Repository\DragonTreasureRepository;
 use Carbon\Carbon;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
 #[ApiResource(
@@ -23,6 +24,12 @@ use Doctrine\ORM\Mapping as ORM;
         new Post(),
         new Put(uriTemplate: 'treasures/{id}.{_format}'),
         new Patch(),
+    ],
+    normalizationContext: [
+        'groups' => ['treasure:read']
+    ],
+    denormalizationContext: [
+        'groups' => ['treasure:write']
     ]
 )]
 class DragonTreasure
@@ -33,25 +40,30 @@ class DragonTreasure
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['treasure:read', 'treasure:write'])]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups('treasure:read')]
     private ?string $description = null;
 
     /**
      * The estimated value of this treasure, in gold coins
      */
     #[ORM\Column]
+    #[Groups(['treasure:read', 'treasure:write'])]
     private ?int $value = null;
 
     #[ORM\Column]
+    #[Groups(['treasure:read', 'treasure:write'])]
     private ?int $coolFactor = null;
 
     #[ORM\Column]
+    #[Groups('treasure:read')]
     private ?\DateTimeImmutable $plunderedAt = null;
 
     #[ORM\Column]
-    private ?bool $isPublished;
+    private ?bool $isPublished = false;
 
     public function __construct()
     {
@@ -80,12 +92,14 @@ class DragonTreasure
         return $this->description;
     }
 
-    public function setDescription(string $description): static
+    public function settDescription(string $description): static
     {
         $this->description = $description;
 
         return $this;
     }
+
+    #[Groups('treasure:write')]
     public function setTextDescription(string $description): static
     {
         $this->description = nl2br($description);
@@ -137,6 +151,14 @@ class DragonTreasure
         return $this->plunderedAt;
     }
 
+    public function setPlunderedAt(\DateTimeImmutable $plunderedAt): static
+    {
+        $this->plunderedAt = $plunderedAt;
+
+        return $this;
+    }
+
+    #[Groups('treasure:read')]
     /**
      * A human-readable representation of when this treasure was plundered.
      */
